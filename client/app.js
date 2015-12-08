03b06d4f59db99dedee55397d8cc2ff913e61c7a
ImmaDragon <devindewitt8@gmail.com>
2015-12-07 20:32:54 -0500

    step4

--- client/app.js
+++ client/app.js
@@ -6,4 +6,7 @@ angular.module('socially',['angular-meteor']);
 angular.module('socially').controller('PartiesListCtrl', function($scope, $meteor){
     $scope.parties = $meteor.collection(Parties);
 
+    $scope.remove = function(party){
+        $scope.parties.remove(party);
+    };
 });
