6ae61474025c874bc63b3d7b3388d1f6bc79f792
ImmaDragon <devindewitt8@gmail.com>
2015-12-07 20:16:54 -0500

    step4

--- client/app.js
+++ client/app.js
@@ -1,1 +1,9 @@
+Parties = new Mongo.Collection('parties');
+
 angular.module('socially',['angular-meteor']);
+//The parties list ctrl controller attaches the party data to the $scope that was injected into our controller fucntion
+//$meteor tag to service the controller with dependency injection
+angular.module('socially').controller('PartiesListCtrl', function($scope, $meteor){
+    $scope.parties = $meteor.collection(Parties);
+
+});
